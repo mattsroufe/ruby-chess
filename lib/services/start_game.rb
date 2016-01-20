7724bd3f21626b39b2f2de4fc42b2a800b3dd50a
Sam Minns <sam.minns@powershop.co.nz>
2016-01-21 11:52:39 +1300

    Add init collections for white and black pieces

--- lib/services/start_game.rb
+++ lib/services/start_game.rb
@@ -1,5 +1,41 @@
 class StartGame
-  POSITIONS = {::Castle => 'a1', ::Bishop => 'a2'}
+  POSITIONS = { 'white' => {
+                  Pawn => a2,
+                  Pawn => b2,
+                  Pawn => c2,
+                  Pawn => d2,
+                  Pawn => e2,
+                  Pawn => f2,
+                  Pawn => g2,
+                  Pawn => h2,
+                  Castle => a1,
+                  Knight => b1,
+                  Bishop => c1,
+                  King => d1,
+                  Queen => e1,
+                  Bishop => f1,
+                  Knight => g1,
+                  Castle => h1,
+                  },
+                'black' => {
+                  Pawn => a7,
+                  Pawn => b7,
+                  Pawn => c7,
+                  Pawn => d7,
+                  Pawn => e7,
+                  Pawn => f7,
+                  Pawn => g7,
+                  Pawn => h7,
+                  Castle => a8,
+                  Knight => b8,
+                  Bishop => c8,
+                  King => d8,
+                  Queen => e8,
+                  Bishop => f8,
+                  Knight => g8,
+                  Castle => h8,
+                }
+              }
 
   def call
     POSITIONS.map do |k, v|
