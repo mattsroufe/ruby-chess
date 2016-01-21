5d490ab2735da37a81dcd1db10cd10f9d198aa6b
Matt Sroufe <mattsroufe@gmail.com>
2016-01-21 15:08:32 +1300

    adds Castle#possible_moves

--- lib/concepts/castle.rb
+++ lib/concepts/castle.rb
@@ -1,2 +1,8 @@
 class Castle < Piece
+
+  def possible_moves
+    rank_moves = (Board::RANKS - [rank]).map { |rank| [file, rank].join }
+    file_moves = (Board::FILES - [file]).map { |file| [file, rank].join }
+    rank_moves + file_moves
+  end
 end
