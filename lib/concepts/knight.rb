9429dda77372e976c6338460ee619e82a3c80619
Sam Minns <sam.minns@powershop.co.nz>
2016-01-21 14:57:41 +1300

    Add HOR_POS to piece and Knight spec for possible moves

--- lib/concepts/knight.rb
+++ lib/concepts/knight.rb
@@ -1,2 +1,19 @@
 class Knight < Piece
+  def possible_moves
+    moves = Set.new
+    x = HOR_POS.index_of(@positon[0])
+    y = @position[1]
+
+    (-2..2).step(2).each do |move_one|
+      (-1..1).each do |move_two|
+        move =  "#{self.rank + move_one} #{self.file + move_two}"
+                "#{self.rank + move_one} #{self.file - move_two}"
+        moves.add()
+      end
+    end
+  end
+
+  def illegal?
+    return
+  end
 end
