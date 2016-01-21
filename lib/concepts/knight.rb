88ca1b2c5cadca3af950ef467c4e0658fd0e1603
Sam Minns <sam.minns@powershop.co.nz>
2016-01-21 17:23:17 +1300

    Add spec for knight moving

--- lib/concepts/knight.rb
+++ lib/concepts/knight.rb
@@ -1,19 +1,32 @@
+require 'Set'
+
 class Knight < Piece
   def possible_moves
     moves = Set.new
-    x = HOR_POS.index_of(@positon[0])
-    y = @position[1]
+    file = Piece::HOR_POS.index(self.position[0]) + 1
+    rank = self.position[1].to_i
 
-    (-2..2).step(2).each do |move_one|
-      (-1..1).each do |move_two|
-        move =  "#{self.rank + move_one} #{self.file + move_two}"
-                "#{self.rank + move_one} #{self.file - move_two}"
-        moves.add()
-      end
-    end
+    moves.add("#{Piece::HOR_POS[(file + 2) - 1]}#{rank + 1}") unless invalid_file?((file + 2) - 1)
+    moves.add("#{Piece::HOR_POS[(file + 2) - 1]}#{rank - 1}") unless invalid_file?((file + 2) - 1)
+    moves.add("#{Piece::HOR_POS[(file - 2) - 1]}#{rank + 1}") unless invalid_file?((file - 2) - 1)
+    moves.add("#{Piece::HOR_POS[(file - 2) - 1]}#{rank - 1}") unless invalid_file?((file - 2) - 1)
+    moves.add("#{Piece::HOR_POS[(file + 1) - 1]}#{rank + 2}") unless invalid_file?(file + 1)
+    moves.add("#{Piece::HOR_POS[(file + 1) - 1]}#{rank - 2}") unless invalid_file?(file + 1)
+    moves.add("#{Piece::HOR_POS[(file - 1) - 1]}#{rank + 2}") unless invalid_file?(file - 1)
+    moves.add("#{Piece::HOR_POS[(file - 1) - 1]}#{rank - 2}") unless invalid_file?(file - 1)
+
+    moves.reject! { |move| move.length != 2 }
+    valid_moves(moves.to_a)
   end
 
-  def illegal?
-    return
+  def invalid_file?(file)
+    file < 0
+  end
+
+  def valid_moves(all_possible_moves)
+    all_possible_moves.reject! { |move| move[1].to_i == self.rank }
+    all_possible_moves.reject! { |move| move[0] == self.file }
+    all_possible_moves.select! { |move| ("a".."h").include?(move[0]) && (1..8).include?(move[1].to_i) }
+    all_possible_moves
   end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,3 +1,20 @@
+require 'spec_helper'
+
 describe 'Knight' do
-  
+  describe '#possible_moves' do
+    context 'from the initial position' do
+      it 'returns only three possible moves from g1' do
+        expect(Knight.new("g1").possible_moves.length).to eq 3
+      end
+      it 'returns only three possible moves from b1' do
+        expect(Knight.new("b1").possible_moves.length).to eq 3
+      end
+      it 'returns only three possible moves from g8' do
+        expect(Knight.new("g8").possible_moves.length).to eq 3
+      end
+      it 'returns only three possible moves from b8' do
+        expect(Knight.new("b8").possible_moves.length).to eq 3
+      end
+    end
+  end
 end
