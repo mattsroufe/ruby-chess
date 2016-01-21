e80d2d4277b56a73d8ad911bf831801c364e33f8
Matthew Gray <matthew.gray@powershop.co.nz>
2016-01-22 11:11:08 +1300

    generalise api calls

--- lib/services/slack.rb
+++ lib/services/slack.rb
@@ -7,7 +7,10 @@ class Slack
   # TOKEN = "xoxb-19012836615-WkvbEtky41yYHGvq7gsJY4nW" # Bots token but only works with the API not RTM
 
   def real_time_session
-    get('rtm.start')
+    api('rtm.start')
+  end
+
+  def join_channel(channel)
   end
 
   def game_channels
@@ -17,10 +20,10 @@ class Slack
   end
 
   def send_message(message, channel_name)
-    channel_id = find_channel_id(channel_name)
-    resource = "chat.postMessage"
-    args = "channel=#{channel_id}&text="+message
-    api(resource, args)
+    api('chat.postMessage', {
+      channel: find_channel_id(channel_name),
+      text:    message,
+    })
   end
 
   def find_channel_id(channel_name)
@@ -30,7 +33,7 @@ class Slack
   private
 
   def channel_list
-    data = get('channels.list')
+    data = api('channels.list')
     data["channels"].map do |channel|
       slice('id', 'name', channel)
     end
@@ -42,15 +45,16 @@ class Slack
     end
   end
 
-  def get(resource)
-    uri = URI("https://slack.com/api/#{resource}?token=#{TOKEN}")
+  def api(resource, options = {})
+    uri = URI("https://slack.com/api/#{resource}?#{params_for(options)}")
     response = Net::HTTP.get(uri)
     JSON.parse(response)
   end
 
-  def api(resource, args)
-    uri = URI("https://slack.com/api/#{resource}?token=#{TOKEN}&"+args)
-    Net::HTTP.get(uri)
+  def params_for(options)
+    options.merge({token: TOKEN})
+           .map { |k,v| "#{k}=#{v}" }
+           .join('&')
   end
 end
 
