343e68095608a4aac8213bf5fe9d517a993affc5
Matt Sroufe <mattsroufe@gmail.com>
2016-01-21 13:01:38 +1300

    adds pieces to game object on StartGame#call

--- lib/services/start_game.rb
+++ lib/services/start_game.rb
@@ -1,45 +1,57 @@
 class StartGame
-  POSITIONS = { 'white' => {
-                  Pawn => a2,
-                  Pawn => b2,
-                  Pawn => c2,
-                  Pawn => d2,
-                  Pawn => e2,
-                  Pawn => f2,
-                  Pawn => g2,
-                  Pawn => h2,
-                  Castle => a1,
-                  Knight => b1,
-                  Bishop => c1,
-                  King => d1,
-                  Queen => e1,
-                  Bishop => f1,
-                  Knight => g1,
-                  Castle => h1,
-                  },
-                'black' => {
-                  Pawn => a7,
-                  Pawn => b7,
-                  Pawn => c7,
-                  Pawn => d7,
-                  Pawn => e7,
-                  Pawn => f7,
-                  Pawn => g7,
-                  Pawn => h7,
-                  Castle => a8,
-                  Knight => b8,
-                  Bishop => c8,
-                  King => d8,
-                  Queen => e8,
-                  Bishop => f8,
-                  Knight => g8,
-                  Castle => h8,
-                }
-              }
+  attr_reader :game
+
+  POSITIONS = {
+    'white' => {
+      Pawn   => 'a2',
+      Pawn   => 'b2',
+      Pawn   => 'c2',
+      Pawn   => 'd2',
+      Pawn   => 'e2',
+      Pawn   => 'f2',
+      Pawn   => 'g2',
+      Pawn   => 'h2',
+      Castle => 'a1',
+      Knight => 'b1',
+      Bishop => 'c1',
+      King   => 'd1',
+      Queen  => 'e1',
+      Bishop => 'f1',
+      Knight => 'g1',
+      Castle => 'h1',
+    },
+    'black' => {
+      Pawn   => 'a7',
+      Pawn   => 'b7',
+      Pawn   => 'c7',
+      Pawn   => 'd7',
+      Pawn   => 'e7',
+      Pawn   => 'f7',
+      Pawn   => 'g7',
+      Pawn   => 'h7',
+      Castle => 'a8',
+      Knight => 'b8',
+      Bishop => 'c8',
+      King   => 'd8',
+      Queen  => 'e8',
+      Bishop => 'f8',
+      Knight => 'g8',
+      Castle => 'h8',
+    }
+  }
+
+  def initialize(str = "")
+    @str = str
+    @game = Game.new
+  end
 
   def call
-    POSITIONS.map do |k, v|
-      k.new(v)
+    %w[white black].each_with_index do |color, i|
+      POSITIONS[color].each do |piece, position|
+        game.players[i].pieces << piece.new(position)
+      end
     end
+    game
+    # @str.split("\n")
   end
 end
